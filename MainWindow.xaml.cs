1ad0223df135c35dedc9423538911893a293a2e1
SLAB66 <SLAB66@KM102-12>
2024-11-05 12:00:59 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace alex_druzhartmann_weatherapp_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetWeather();
+        }
+
+        public async Task<string> GetWeather()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://www.prevision-meteo.ch/services/json/Annecy");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Root root = JsonConvert.DeserializeObject<Root>(content);
+                CityInfo city_info = root.city_info;
+                string city = city_info.name;
+                TB_City.Text = city;
+            }
+            return null;
+        }
+    }
+}
